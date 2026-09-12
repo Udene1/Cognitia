68d4ca1454d4515cb403bba48347547d0f813d95
Udene1 <157874312+Udene1@users.noreply.github.com>
2026-09-12 17:27:57 -0400

    Export structural discovery and search learning

--- cognitia/__init__.py
+++ cognitia/__init__.py
@@ -16,9 +16,10 @@ from .discovery import DiscoveryWorkspace, ExplanationAssessment, ExplanationSta
 from .discovery_experiments import DiscriminatingExperimentSelector, Experiment
 from .discovery_hypotheses import ExplanatoryModel, HypothesisAlternative, HypothesisSpaceBuilder, HypothesisTransform
 from .discovery_prediction import Prediction, PredictionDeriver
+from .discovery_structure import ModelElement, ModelRelation, RelationKind, StructuralAlternative, StructuralHypothesisBuilder, StructuralModel
 from .failure_analysis import CapabilityGapDiagnosis, FailureAnalyzer, FailureClass, FailureObservation
 from .git_environment import GitCommitObservation, GitEnvironmentError, GitHistoryIngestor, GitRepositoryObserver
-from .learning import PersistentPatternLearner
+from .learning import HypothesisSearchLearner, PersistentPatternLearner, SearchStrategy
 from .git_knowledge import GitKnowledgeIngestor
 from .learning.transfer import TransferAssessment, assess_transfer
 from .promotion import CognitivePromotionOrchestrator, PromotionEvaluation
@@ -42,9 +43,10 @@ __all__ = [
     "DiscoveryWorkspace", "ExplanationAssessment", "ExplanationStatus", "ExplanatoryGap", "HypothesisCandidate", "Observation", "unresolved_observations",
     "ExplanatoryModel", "HypothesisAlternative", "HypothesisSpaceBuilder", "HypothesisTransform",
     "Prediction", "PredictionDeriver", "DiscriminatingExperimentSelector", "Experiment",
+    "ModelElement", "ModelRelation", "RelationKind", "StructuralAlternative", "StructuralHypothesisBuilder", "StructuralModel",
     "CognitiveStateReplayer", "RecoveredCognitiveState",
     "GitCommitObservation", "GitEnvironmentError", "GitHistoryIngestor", "GitRepositoryObserver",
-    "GitKnowledgeIngestor", "PersistentPatternLearner",
+    "GitKnowledgeIngestor", "PersistentPatternLearner", "HypothesisSearchLearner", "SearchStrategy",
     "FailureClass", "FailureObservation", "FailureAnalyzer", "CapabilityGapDiagnosis",
     "TransferAssessment", "assess_transfer",
     "VerificationOutcome", "VerificationPlan", "VerificationResult", "VerificationStep", "execute_plan",
