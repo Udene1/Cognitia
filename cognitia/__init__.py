ba72f401e9e2e4cde7e7ada4c65a0e6883e0a737
Udene1 <157874312+Udene1@users.noreply.github.com>
2026-09-12 07:37:12 -0400

    feat: expose capability acquisition primitives

--- cognitia/__init__.py
+++ cognitia/__init__.py
@@ -1,3 +1,18 @@
 """Cognitia: experimental artificial cognitive system."""
 
+from .acquisition import (
+    AcquisitionProposal,
+    AcquisitionStage,
+    CapabilityRequirement,
+    propose_capability_acquisition,
+)
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "AcquisitionProposal",
+    "AcquisitionStage",
+    "CapabilityRequirement",
+    "propose_capability_acquisition",
+    "__version__",
+]
