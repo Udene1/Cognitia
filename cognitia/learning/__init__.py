2ac365a81c82f8fbeb5514e42f0d64916d945590
Udene1 <157874312+Udene1@users.noreply.github.com>
2026-09-12 13:15:08 -0400

    Expose reasoning pattern learning

--- cognitia/learning/__init__.py
+++ cognitia/learning/__init__.py
@@ -3,12 +3,15 @@
 from .conditional import ConditionalPattern, ConditionalPatternLearner
 from .model_revision import RevisionAction, RevisionDecision, revise_hypothesis
 from .persistent_patterns import PersistentPatternLearner
+from .reasoning_patterns import ReasoningPattern, ReasoningPatternLearner
 from .scientific import Hypothesis, ScientificEvaluator, TestResult
 
 __all__ = [
     "ConditionalPattern",
     "ConditionalPatternLearner",
     "PersistentPatternLearner",
+    "ReasoningPattern",
+    "ReasoningPatternLearner",
     "Hypothesis",
     "ScientificEvaluator",
     "TestResult",
