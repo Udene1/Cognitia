e9d58e8082beaae3c3b8af15bb0fe07504627332
Udene1 <157874312+Udene1@users.noreply.github.com>
2026-09-12 13:33:46 -0400

    feat: export durable knowledge store

--- cognitia/knowledge/__init__.py
+++ cognitia/knowledge/__init__.py
@@ -1,6 +1,13 @@
 """Knowledge acquisition and structured knowledge representation."""
 
 from .model import KnowledgeItem, KnowledgeSource
+from .persistent import KnowledgePersistenceError, PersistentKnowledgeStore
 from .store import KnowledgeStore
 
-__all__ = ["KnowledgeItem", "KnowledgeSource", "KnowledgeStore"]
+__all__ = [
+    "KnowledgeItem",
+    "KnowledgeSource",
+    "KnowledgeStore",
+    "KnowledgePersistenceError",
+    "PersistentKnowledgeStore",
+]
