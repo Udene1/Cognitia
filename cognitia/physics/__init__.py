bd9e4d4104ba4dc87f69ba31433a2a3bfa98b9ca
Udene1 <157874312+Udene1@users.noreply.github.com>
2026-09-12 12:31:47 -0400

    feat: expose model evidence evaluation

--- cognitia/physics/__init__.py
+++ cognitia/physics/__init__.py
@@ -3,6 +3,7 @@
 from .dynamics import DynamicsPrediction, NewtonianDynamics
 from .mechanics import KinematicState, KinematicsModel, Prediction
 from .measurement import Measurement, PredictionError, PredictionVerdict, Uncertainty, compare_prediction
+from .model_evaluation import ModelEvaluation, ModelEvidence, evaluate_prediction
 from .newton import NewtonPrediction, NewtonianModel
 from .quantities import (
     ACCELERATION,
@@ -21,4 +22,5 @@ __all__ = [
     "Dimension", "Quantity", "KinematicState", "KinematicsModel", "Prediction",
     "NewtonPrediction", "NewtonianModel", "DynamicsPrediction", "NewtonianDynamics",
     "Measurement", "Uncertainty", "PredictionError", "PredictionVerdict", "compare_prediction",
+    "ModelEvaluation", "ModelEvidence", "evaluate_prediction",
 ]
