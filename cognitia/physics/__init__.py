225873673fd38eecf90b6a65ea24ca0fe378d6a8
Udene1 <157874312+Udene1@users.noreply.github.com>
2026-09-12 06:31:17 -0400

    Add physics domain package

---
+++ cognitia/physics/__init__.py
@@ -0,0 +1,5 @@
+"""Foundational physical models used by Cognitia for prediction and testing."""
+
+from .mechanics import KinematicState, KinematicsModel, Prediction
+
+__all__ = ["KinematicState", "KinematicsModel", "Prediction"]
