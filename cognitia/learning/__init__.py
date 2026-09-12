3798bace832eb3a8f21ede6d1de7528aa43ec984
Udene1 <157874312+Udene1@users.noreply.github.com>
2026-09-12 06:20:45 -0400

    Add conditional learning package

---
+++ cognitia/learning/__init__.py
@@ -0,0 +1,5 @@
+"""Learning mechanisms built from Cognitia's accumulated experience."""
+
+from .conditional import ConditionalPattern, ConditionalPatternLearner
+
+__all__ = ["ConditionalPattern", "ConditionalPatternLearner"]
