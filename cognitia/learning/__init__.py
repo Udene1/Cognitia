728a6c02fefc9abab3c0bc1e544841c6e73c6991
Udene1 <157874312+Udene1@users.noreply.github.com>
2026-09-12 13:12:42 -0400

    feat: export persistent pattern learner

--- cognitia/learning/__init__.py
+++ cognitia/learning/__init__.py
@@ -2,11 +2,13 @@
 
 from .conditional import ConditionalPattern, ConditionalPatternLearner
 from .model_revision import RevisionAction, RevisionDecision, revise_hypothesis
+from .persistent_patterns import PersistentPatternLearner
 from .scientific import Hypothesis, ScientificEvaluator, TestResult
 
 __all__ = [
     "ConditionalPattern",
     "ConditionalPatternLearner",
+    "PersistentPatternLearner",
     "Hypothesis",
     "ScientificEvaluator",
     "TestResult",
