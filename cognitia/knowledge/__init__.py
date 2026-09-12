fb3befed831d4004f1e8c41c788bdb840b99eb61
Udene1 <157874312+Udene1@users.noreply.github.com>
2026-09-12 06:12:07 -0400

    Add structured knowledge package

---
+++ cognitia/knowledge/__init__.py
@@ -0,0 +1,6 @@
+"""Knowledge acquisition and structured knowledge representation."""
+
+from .model import KnowledgeItem, KnowledgeSource
+from .store import KnowledgeStore
+
+__all__ = ["KnowledgeItem", "KnowledgeSource", "KnowledgeStore"]
