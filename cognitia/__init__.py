58e128e0b52a4504accfd4cc8fbf1802335aaab6
Udene1 <157874312+Udene1@users.noreply.github.com>
2026-09-16 01:05:39 -0400

    Export recipient context research primitive

--- cognitia/__init__.py
+++ cognitia/__init__.py
@@ -44,7 +44,7 @@ from .verification import VerificationOutcome, VerificationPlan, VerificationRes
 from .logic_ir import LogicModel, LogicNode, LogicRelation, LogicProvenance, LogicTransferCandidate, LogicTransferEngine, LogicTransferVerification, TransferMapping, model_from_parts
 from .logic_adapters import CodeLogicAdapter, LogicAdapterResult, MathLogicAdapter, TextLogicAdapter, UniversalLogicAdapter
 from .learning.concept_transfer import GenericLogicExtractor
-from .communicative_cognition import CommunicativeAct, CommunicativeDecision, CommunicativeObjective, CognitiveCommunicationState, EpistemicPreservationError, HypothesisState, InteractionContext, assert_epistemic_preservation, select_communicative_act
+from .communicative_cognition import CommunicativeAct, CommunicativeDecision, CommunicativeObjective, CognitiveCommunicationState, EpistemicPreservationError, HypothesisState, InteractionContext, RecipientRole, assert_epistemic_preservation, select_communicative_act
 
 __version__ = "0.1.0"
 
@@ -60,5 +60,5 @@ __all__ = [
     "CognitiveStateReplayer", "RecoveredCognitiveState", "GitCommitObservation", "GitEnvironmentError", "GitHistoryIngestor", "GitRepositoryObserver", "GitKnowledgeIngestor", "PersistentPatternLearner", "HypothesisSearchLearner", "SearchStrategy", "KnowledgeItem", "KnowledgeSource", "KnowledgeTest", "ValidatedKnowledgeStore",
     "TextToken", "EntityMention", "EventMention", "RelationMention", "LanguageFrame", "SemanticProposition", "QuestionAnalysis", "AnswerContract", "build_language_frame", "analyze_question", "AnswerAssessment", "AnswerContractPlanner", "AnswerPlan", "plan_answer",
     "FailureClass", "FailureObservation", "FailureAnalyzer", "CapabilityGapDiagnosis", "TransferAssessment", "assess_transfer", "VerificationOutcome", "VerificationPlan", "VerificationResult", "VerificationStep", "execute_plan", "CandidateDisposition", "PromotionDecision", "RegressionFinding", "RegressionPolicy", "evaluate_promotion", "CognitivePromotionOrchestrator", "PromotionEvaluation",
-    "LogicModel", "LogicNode", "LogicRelation", "LogicProvenance", "LogicTransferCandidate", "LogicTransferEngine", "LogicTransferVerification", "TransferMapping", "model_from_parts", "LogicAdapterResult", "TextLogicAdapter", "CodeLogicAdapter", "MathLogicAdapter", "UniversalLogicAdapter", "GenericLogicExtractor", "CommunicativeAct", "CommunicativeDecision", "CommunicativeObjective", "CognitiveCommunicationState", "EpistemicPreservationError", "HypothesisState", "InteractionContext", "assert_epistemic_preservation", "select_communicative_act", "__version__",
+    "LogicModel", "LogicNode", "LogicRelation", "LogicProvenance", "LogicTransferCandidate", "LogicTransferEngine", "LogicTransferVerification", "TransferMapping", "model_from_parts", "LogicAdapterResult", "TextLogicAdapter", "CodeLogicAdapter", "MathLogicAdapter", "UniversalLogicAdapter", "GenericLogicExtractor", "CommunicativeAct", "CommunicativeDecision", "CommunicativeObjective", "CognitiveCommunicationState", "EpistemicPreservationError", "HypothesisState", "InteractionContext", "RecipientRole", "assert_epistemic_preservation", "select_communicative_act", "__version__",
 ]
